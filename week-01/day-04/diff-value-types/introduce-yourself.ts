aec9e9c9e1ee331bb3441882b0e9ecdd27743b85
nldanne <nldanne97@gmail.com>
2021-09-30 16:40:44 +0200

    introducing me with variables

---
+++ week-01/day-04/diff-value-types/introduce-yourself.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line:
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
+
+var fName: string = 'Anne';
+var sName: string = 'Nguyen';
+var age: number = 24;
+var height: number = 1.53;
+
+console.log(fName);
+console.log(sName);
+console.log(age);
+console.log(height);
