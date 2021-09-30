99b17d1ce232d5600c91c6c241dd4c7c2c5a93a9
nldanne <nldanne97@gmail.com>
2021-09-30 15:46:46 +0200

    calculating the sides of a cuboid

---
+++ week-01/day-04/cuboid.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+
+let side1: number;
+let side2: number;
+let side3: number;
+let surfaceArea: number = 600;
+let volume: number = 1000;
+
+side3 = 1000 / 600;
+side1 = Math.sqrt(600); //square root
+side2 = side1;
+console.log(side3);
+console.log(side2);
+console.log(side1);
+
