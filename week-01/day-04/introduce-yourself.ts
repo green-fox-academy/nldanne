ac17784adcdd2a8e2c16cea4aa0343c9a7cc15b3
nldanne <nldanne97@gmail.com>
2021-09-30 13:52:13 +0200

    introducing myself

---
+++ week-01/day-04/introduce-yourself.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line:
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
+
+var firstName = 'Anne';
+var secondName = 'Nguyen';
+var age = 24;
+var height = 1.53;
+
+console.log(firstName);
+console.log(secondName);
+console.log(age);
+console.log(height);
