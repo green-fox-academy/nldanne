3fdc72aab1437855cd57a87f77ce9b285a5ad138
nldanne <nldanne97@gmail.com>
2021-09-30 14:06:02 +0200

    using mathematical operations with two nums

---
+++ week-01/day-04/two-numbers.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+var num1: number = 22;
+var num2: number = 13;
+
+// Print the result of 13 added to 22
+console.log(num2 + num1);  //Prints: 35
+
+// Print the result of 13 substracted from 22
+console.log(num1 - num2);  //Prints: 9
+
+// Print the result of 22 multiplied by 13
+console.log(num1 * num2);  //Prints: 286
+
+// Print the result of 22 divided by 13 (as a decimal fraction)
+console.log(num1 / num2);  //Prints: 1.6923076923076923
+
+// Print the remainder of 22 divided by 13
+console.log(num1 % num2);  //Prints: 9
+
