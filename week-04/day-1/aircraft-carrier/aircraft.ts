9a68f0ad02d620b746b4b0f8ebf1590b0562d642
nldanne <nldanne97@gmail.com>
2021-10-25 16:49:55 +0200

    created a base aircraft class

---
+++ week-04/day-1/aircraft-carrier/aircraft.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+/*
+    Create a class that represents an aircraft
+    There are 2 types of aircrafts: F16 and F35
+    Both aircrafts should keep track of their ammunition
+*/
+
+export class Aircraft {
+    protected maxAmmo: number;
+    protected baseDamage: number;
+
+    constructor() {
+        this.maxAmmo = 0;
+        this.baseDamage = 0;
+    }
+
+}
