d9cf0e736ce76e9f29b06d7cadc58f2013850e06
nldanne <nldanne97@gmail.com>
2021-10-25 16:51:46 +0200

    f35 extends aircraft class

---
+++ week-04/day-1/aircraft-carrier/f35.ts
@@ -0,0 +1,59 @@
+'use strict';
+
+
+import { Aircraft } from "./aircraft";
+
+export class F35 extends Aircraft {
+  protected type: string;
+  protected currentAmmo: number;
+
+  constructor() {
+    super();
+    this.type = 'F35';
+    this.currentAmmo = 0;
+    this.maxAmmo = 12;
+    this.baseDamage = 50;
+  }
+
+  public fight() :number{
+    this.currentAmmo = 0;
+    return this.baseDamage * this.maxAmmo;
+  }
+
+  public refillAmmo(ammunition: number) :number {
+    if(this.currentAmmo === this.maxAmmo) {
+      return ammunition;
+
+    } else {
+        if(ammunition <= this.maxAmmo) {
+          this.currentAmmo += ammunition;
+          return 0;
+
+        } else {
+          this.currentAmmo += this.maxAmmo;
+          return ammunition - this.maxAmmo;
+        }   
+    }
+  }
+
+
+  public getType() :string {
+    return this.type;
+  }
+
+
+  public getStatus() :string {
+    return (`Type ${this.type}, Ammo: ${this.currentAmmo}, Base Damage: ${this.baseDamage}, All Damage: ${this.fight()}`);
+  }
+
+
+  public isPriority() :boolean{
+    return true;
+  }
+}
+
+let myBoom = new F35();
+console.log(myBoom);
+
+console.log(myBoom.refillAmmo(30));
+console.log(myBoom.getStatus());
