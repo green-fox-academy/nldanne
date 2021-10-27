ac334a848e5a1b3f26cd74222dbde72ebdf60a98
nldanne <nldanne97@gmail.com>
2021-10-27 12:19:12 +0200

    added try and catch method

--- week-03/day-2/duplicated-chars.ts
+++ week-03/day-2/duplicated-chars.ts
@@ -9,31 +9,27 @@
 
 const fs = require('fs');
 
-try {
-    let result: string = decryptDoubled('duplicated-chars.txt');
-    /*let output: string*/
-    fs.writeFileSync('D:/greenfox/nldanne/week-03/day-02/single-chars.txt', result);
-    fs.open('D:/greenfox/nldanne/week-03/day-02/single-chars.txt',  )
-    
-
-} catch {
-
-}
+let myFile: string = 'duplicated-chars.txt';
 
 function decryptDoubled(duplicateContent: string) {
-    let newContent: string = '';
-    let singleContent = fs.readFileSync(duplicateContent, 'utf8');
+  let single: string = '';
+  let duplicated: string;
+    
+  try {
+    duplicated = fs.readFileSync(duplicateContent, 'utf8');
+  } catch {
+    throw ('File not found');
+  }
 
-    for (var i=0; i< singleContent.length; i = i + 2) {
-        if(singleContent[i] == '.') {
-          newContent = newContent + singleContent[i] + '\n';
-          //  console.log(singleContent[i]);
-        }else {
-          newContent = newContent + singleContent[i];
-        }
-      }
-    return newContent;
+  for (var i=0; i< duplicated.length; i = i + 2) {
+    single = single + duplicated[i];
+  }
+
+  fs.writeFileSync('output.txt', single);
+  return single;
 }
+
+decryptDoubled(myFile);
 
 //let result = decryptDoubled('duplicated-chars.txt');
 //console.log(result);
