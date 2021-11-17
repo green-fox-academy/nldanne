2760ec3fd5634efc29e97dea3b350d755efd7ed9
nldanne <nldanne97@gmail.com>
2021-11-17 18:13:38 +0100

    code to use int Postman for POST request

---
+++ week-07/day-3/array-handler/postman.js
@@ -0,0 +1,22 @@
+// POST / arrays
+// receives a json object:
+/*
+  {
+    "what": "sum",
+    "numbers": [1, 2, 5, 10]
+  }
+ */
+
+  /*
+  {
+    "what": "multiply",
+    "numbers": [1, 2, 5, 10]
+  }
+ */
+
+  /*
+  {
+    "what": "double",
+    "numbers": [1, 2, 5, 10]
+  }
+ */
