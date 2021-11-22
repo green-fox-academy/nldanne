08896b8cf4cc1ad9cdc7fa311c41704f8d5f7376
nldanne <nldanne97@gmail.com>
2021-11-22 17:42:45 +0100

    working post request with minimal error handling

--- week-07/day-5/app.js
+++ week-07/day-5/app.js
@@ -32,7 +32,7 @@ app.get('/hello', (req, res) => {
 
 // GET all posts
 app.get('/posts', (req, res) => {
-  const SQL_GET_POSTS_QUERY = 'SELECT * FROM posts;';
+  const SQL_GET_POSTS_QUERY = 'SELECT id, title, url, timestamp, score, user_name AS owner FROM posts INNER JOIN users ON posts.user_id = users.user_id;';
 
   conn.query(SQL_GET_POSTS_QUERY, (error, rows) => {
     if (error) {
@@ -61,38 +61,62 @@ app.get('/posts', (req, res) => {
 
 // create a new reddit post
 app.post('/posts', (req, res) => {
-  const SQ_INSERT_POST_QUERY = 'INSERT INTO posts (title, url) VALUES (?, ?);';  
-  
-  let newId = null;
+  let userName = req.headers.user_name;
+  const SQL_GET_ID_QUERY = `SELECT user_id FROM users WHERE user_name = ?;`;
 
-  if (req.headers['content-type'] !== 'application/json') {
-    res.status(400).json('Please provide correct json content!');
+  if(!userName) {
+    res.status(400).json('Please provide a user_name!');
     return;
   }
 
-  conn.query(SQ_INSERT_POST_QUERY, [req.body.title, req.body.url], (error, rows) => {
+  conn.query(SQL_GET_ID_QUERY, [userName], (error, rows) => {
     if (error) {
       console.log(error);
-      res.status(500).json('INTERNAL SERVER ERROR 2');
+      res.status(500).json('INTERNAL SERVER ERROR Hello');
       return;
     }
 
-    // responds with the json of the newly created reddit post
-    
-    newId = rows.insertId;
-    const SQL_SELECT_POST_QUERY = `SELECT * FROM posts WHERE id=?;`;
+    let userId = rows[0].user_id;
 
-    conn.query(SQL_SELECT_POST_QUERY, [newId], (error, rows) => {
+    //console.log(req.headers);
+    // saves a new post to the database 
+    const SQ_INSERT_POST_QUERY = `INSERT INTO posts (title, url, user_id) VALUES (?, ?, ?);`;  
+    
+    let newId = null;
+  
+    if (req.headers['content-type'] !== 'application/json') {
+      res.status(400).json('Please provide correct json content!');
+      return;
+    }
+  
+    conn.query(SQ_INSERT_POST_QUERY, [req.body.title, req.body.url, userId], (error, rows) => {
       if (error) {
         console.log(error);
-        res.status(500).json('INTERNAL SERVER ERROR 3');
+        res.status(500).json('INTERNAL SERVER ERROR 2');
         return;
       }
+  
+      // responds with the json of the newly created reddit post
+      newId = rows.insertId;
+      const SQL_SELECT_POST_QUERY = `SELECT id, title, url, timestamp, score, user_name AS owner FROM posts INNER JOIN users ON posts.user_id = users.user_id WHERE id=?;`;
+  
+      conn.query(SQL_SELECT_POST_QUERY, [newId], (error, rows) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json('INTERNAL SERVER ERROR 3');
+          return;
+        }
+  
+        res.status(201).json(rows);
+        console.log('new Reddit post was successfully added');
+      })
+  
+    });
+  })
 
-      res.status(201).json(rows);
-      console.log('new Reddit post was successfully added');
-    })
-
-  });
 
 });
+
+//app.put('/posts/:id', (req, res) => {
+//  const SQL_UPDATE_POST_QUERY ;
+//})
