1e2fbcd1232d83a8a31df90f3c263f1f9092380c
nldanne <nldanne97@gmail.com>
2021-12-09 10:03:54 +0100

    added error handling and elements to the DOM

--- week-10/booking/public/manage.js
+++ week-10/booking/public/manage.js
@@ -9,14 +9,26 @@ form.addEventListener('submit', (event) => {
   fetch(`http://localhost:3000/api/booking?lastName=${form.lastName.value}&bookingReference${form.bookingReference.value}`)
   .then(response => response.json())
   .then(data => {
-    let p1 = document.createElement('p');
-    p1.textContent= 'First name: ' + data.firstName;
 
-    let p2 = document.createElement('p');
-    p2.textContent = 'Last name: ' + data.lastName;
+    if(data.message) {
+      let p = document.createElement('p');
+      p.textContent = data.message;
+      return;
+    } else {
 
-    let p3 = document.createElement('p');
-    p3.textContent = 'Passport number: ' + data.passportNumber;
+      let p1 = document.createElement('p');
+      p1.textContent= 'First name: ' + data.firstName;
+  
+      let p2 = document.createElement('p');
+      p2.textContent = 'Last name: ' + data.lastName;
+  
+      let p3 = document.createElement('p');
+      p3.textContent = 'Passport number: ' + data.passportNumber;
+  
+      div.appendChild(p1);
+      div.appendChild(p2);
+      div.appendChild(p3);
+    }
   })
   .catch(error => {
     alert('Something went wrong');
