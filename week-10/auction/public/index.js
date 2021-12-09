aa28634097eb7da963a34119d83906ef17fc48d3
nldanne <nldanne97@gmail.com>
2021-12-09 17:21:37 +0100

    auction exam version 2

---
+++ week-10/auction/public/index.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const ul = document.querySelector('ul');
+const form = document.querySelector('form');
+const select = document.querySelector('select');
+const p = document.querySelector('p');
+
+
+window.addEventListener('load', (event) => {
+  event.preventDefault();
+
+  fetch('http://localhost:3000/api/items')
+    .then((response) => response.json())
+    .then((data) => {
+
+      data.forEach((element) => {
+        let li = document.createElement('li');
+        li.textContent = `${element.title} (highest bid: ${element.highestBid}, ${element.highestBidderName})`;
+        ul.appendChild(li);
+
+        let option = document.createElement('option');
+        option.textContent = element.title;
+        option.setAttribute('value', `${element.title}`);
+        option.setAttribute('id', `${element.id}`);
+        select.appendChild(option);
+      });
+
+    })
+    .catch(error => {
+      alert('Internal server error');
+      console.log(error);
+    })
+});
+
+const valami = document.getElementById('1');
+console.log(valami);
+
+console.log(select);
+console.log(select.selectedIndex);
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if(!form.bidderName.value || form.bidderName.value === '' || !form.amount.value || form.bidderName.value === '') {
+    p.textContent = 'Please fill in the fields';
+  }
+
+  let index = document.querySelector('select').options.selectedIndex;
+
+  fetch(`http://localhost:3000/api/items/:${index}/bids`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      highestBidderName: form.bidderName.value,
+      amount: form.amount.value
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+
+    if(data.message === "The auction is over!") {
+      p.textContent = "The auction is over!";
+      form.reset();
+      return;
+    } 
+
+    if(data.message === "Your bid is below the highest bid!") {
+      p.textContent === "Your bid is below the highest bid!";
+      return;
+    }
+
+    if(data.messasge === 'Succesfull!') {
+      location.reload();
+      form.reset();
+    }
+  })
+  .catch(error => {
+    alert('Something went wrong');
+    console.log(error);
+  })
+})
---
+++ week-10/auction/server.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const PORT = 3000;
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'auction',
+});
+
+conn.connect((error) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log('Successfully connected to Database');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/api/items/:id/bids', (req, res) => {
+  if(!req.body.bidderName || !req.body.amount) {
+    res.status(400).json({message: 'please fill out all the fields'});
+    return;
+  }
+
+  if(!req.params.id) {
+    res.status(400).json({message: 'Provide and id'});
+    return;
+  }
+
+
+  conn.query('SELECT * FROM stuffs WHERE DATE(expirydate) > CURDATE() AND id = ?', [req.params.id], (err, result) => {
+    if(err) {
+      res.status(500).json({message: 'Internal server error'});
+      return;
+    } else if (result.length === 0) {
+      res.status(400).json({message: 'There is no item like that on the auction'});
+      return;
+    } else if (result[0].expiryDate < currentDate) {
+      //If the auction is over
+      res.status(400).json({message: "The auction is over!"});
+      return;
+    } else {
+      if(req.body.amount <= result[0].highestBid) {
+        res.json(400).json({message: "Your bid is below the highest bid!"});
+        return;
+      } else {
+        conn.query('UPDATE stuffs SET highestBid = ?, highestBidderName = ? WHERE id = ?', [req.body.amount, req.body.bidderName, req.params.id], (err, rows) => {
+          if(err) {
+            res.status(500).json({message: 'Internal server error'});
+            return;
+          } else {
+            res.status(201).json({message: 'Succesfull!'});
+          }
+        });
+      }
+    }
+  });
+});
+
+app.get('/api/items', (req, res) => {
+  conn.query('SELECT * FROM stuffs; ', (err, result) => {
+    if(err) {
+      res.status(500).json({message: 'Internal server error'});
+      return;
+    } else {
+      res.status(200).json(result);
+    }
+  })
+});
+
